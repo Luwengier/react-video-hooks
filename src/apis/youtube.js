781a6418721ab792e73040233eb5b71766ad295c
Luwengier <g217881111@gmail.com>
2021-02-26 22:11:11 +0800

    Add axios and youtube.js with basic setting

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyBVulY7Z1WJLlnvlPmE6c65qHC2fEvZ8OU';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResult: 5,
+    key: KEY
+  }
+});
